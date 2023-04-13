df27f4e0b3558fe9f9bde6db6c6c5adb41fc3984
DevPAN <moisesrb18@gmail.com>
2023-04-12 20:55:11 -0600

    actualizando codigo

--- main.js
+++ main.js
@@ -37,18 +37,11 @@ function encriptar(texto) {
       return llaves[match];
     });
   }
+function desencriptar(texto) {
+    return texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  }
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_expressions
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/match
-
-  
-let textoOriginal = "Hola mundo"
-let textoEncriptado = encriptar(textoOriginal);
-
-//Crear los botones
-console.log(llaves);
-console.log(textoOriginal); // hola mundo
-console.log(textoEncriptado); // hoberlai mufatndober
-
 
 function procesar(){
     let entrada = document.getElementById("textoOriginal");
@@ -57,3 +50,30 @@ function procesar(){
     let resultado = encriptar(texto);
     salida.value = resultado;
 }
+
+function procesarDesencriptar(){
+    let entrada = document.getElementById("textoOriginal");
+    let salida = document.getElementById("textoEncriptado");
+    let texto = entrada.value;
+    let resultado = desencriptar(texto);
+    salida.value = resultado;
+}
+
+function copiarAlPortapapeles() {
+    let texto = document.getElementById("textoEncriptado").value;
+    navigator.clipboard.writeText(texto)
+    .then(() => {
+      console.log("Texto copiado al portapapeles");
+    })
+    .catch((error) => {
+      console.error("Error al copiar el texto: ", error);
+    });
+  }
+
+let textoOriginal = "Hola mundo"
+let textoEncriptado = encriptar(textoOriginal);
+  
+  //Crear los botones
+console.log(llaves);
+console.log(textoOriginal); // hola mundo
+console.log(textoEncriptado); // hoberlai mufatndober
