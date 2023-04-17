b3bd0688881cec1dc7eb7047d9e7303796642ff5
DevPAN <moisesrb18@gmail.com>
2023-04-16 19:37:31 -0600

    Agregada barra de progreso para el toast

--- main.js
+++ main.js
@@ -111,16 +111,35 @@ function mostrarOcultarDivs() {
 
 //Creamos la variable global para almacenar el identificador del temporizador
 let timerId = null;
+//Creamos la variable global para almacenar el identificador del intervalo
+let intervalId = null;
 //Creamos la funcion notificacion y creamos la variable notificacion para asignarle una clase y trabajar en CSS
 function mostrarNotificacion(mensajeTexto) {
   mensaje.innerText = mensajeTexto; //se cambia el texto del párrafo por el que se quiere mostrar
   notificacion.classList.add("show");
   //Cancelamos el temporizador anterior si existe
   clearTimeout(timerId);
+  //Cancelamos el intervalo anterior si existe
+  clearInterval(intervalId);
   //Creamos un nuevo temporizador y guardamos su identificador en la variable timerId
   timerId = setTimeout(function() {
     notificacion.classList.remove("show");
   }, 3000);
+  //Obtenemos el elemento progress dentro de la notificación
+  let progress = document.querySelector("#notificacion progress");
+  //Asignamos el valor máximo al elemento progress
+  progress.max = 3000;
+  //Asignamos el valor actual al elemento progress
+  progress.value = 3000;
+  //Creamos un nuevo intervalo para actualizar el valor del elemento progress cada 10 milisegundos y guardamos su identificador en la variable intervalId
+  intervalId = setInterval(function() {
+    //Reducimos el valor del elemento progress en 10 unidades
+    progress.value -= 10;
+    //Si el valor del elemento progress llega a cero, detenemos el intervalo
+    if (progress.value === 0) {
+      clearInterval(intervalId);
+    }
+  }, 10);
 }
 
 //Cerrar notificaciones
@@ -131,6 +150,10 @@ const cerrar = document.getElementById("cerrar");//usamos el ID del boton cerrar
 cerrar.addEventListener("click", function(event) {
   event.preventDefault();
   notificacion.classList.remove("show");//usamos de nuevo la clase en el evento click
+  //Detenemos el temporizador si existe
+  clearTimeout(timerId);
+  //Detenemos el intervalo si existe
+  clearInterval(intervalId);
 });
 
 
