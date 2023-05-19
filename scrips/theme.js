d7bef76d6f3e2ef53a08e9b93775f0a45fa90514
DevPAN <moisesrb18@gmail.com>
2023-05-19 12:07:48 -0600

    lipeza de codigo

--- scrips/theme.js
+++
@@ -1,5 +0,0 @@
-const themeToggleBtn = document.getElementById("theme-toggle-btn");
-themeToggleBtn.addEventListener("click", function () {
-  document.body.classList.toggle("theme-dark");
-  document.body.classList.toggle("theme-white");
-});
