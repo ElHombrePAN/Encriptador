dea482df07607940b99db1893f68c1c16e4f6c3e
Moises R <moisesrb18@gmail.com>
2024-07-07 12:56:32 -0600

    Ajuste ligero

--- scripts/visibilidad.js
+++ scripts/visibilidad.js
@@ -1,45 +1,30 @@
-// Primero, encontramos el cuadro de texto donde el usuario escribe
+// Referencias a los elementos del DOM
 const inputTextoOriginal = document.getElementById("textarea_uno");
-// Luego, encontramos el cuadro de texto donde se muestra el texto encriptado
 const outputTextoEncriptado = document.getElementById("textarea_dos");
 
-// Cuando el usuario escribe algo en el cuadro de texto original
+// Actualiza el texto encriptado y la visibilidad de las secciones
 inputTextoOriginal.addEventListener("input", () => {
-    // Copiamos lo que el usuario escribió al cuadro de texto encriptado
-    outputTextoEncriptado.value = inputTextoOriginal.value; // Corrección en la referencia
-    // Llamamos a una función para mostrar u ocultar partes de la página
+    outputTextoEncriptado.value = inputTextoOriginal.value;
     actualizarVisibilidad();
 });
 
-
-// Esta es la función que muestra u oculta partes de la página
+// Muestra u oculta secciones y botones en función del texto ingresado
 function actualizarVisibilidad() {
-    // Encontramos lo que el usuario escribió
-    let texto = inputTextoOriginal.value;
-    // Encontramos la sección que dice que no hay ningún mensaje
-    let seccionSinMensaje = document.getElementById('seccion_dos');
-    // Encontramos la sección que muestra el cuadro de texto encriptado
-    let seccionConMensaje = document.getElementById('seccion_tres');
+    const texto = inputTextoOriginal.value;
+    const seccionSinMensaje = document.getElementById('seccion_dos');
+    const seccionConMensaje = document.getElementById('seccion_tres');
 
-    // Si el cuadro de texto original está vacío (no hay nada escrito)
     if (texto === "") {
-        // Mostramos la sección que dice que no hay ningún mensaje
         seccionSinMensaje.style.display = "block";
-        // Ocultamos la sección del cuadro de texto encriptado
         seccionConMensaje.style.display = "none";
         document.getElementById("btn_limpiar").disabled = true;
         document.getElementById("btn_encriptar").disabled = true;
         document.getElementById("btn_desencriptar").disabled = true;
-        //document.getElementById("btn_guardar").disabled = true;
     } else {
-        // Si hay algo escrito, ocultamos la sección que dice que no hay ningún mensaje
         seccionSinMensaje.style.display = "none";
-        // Y mostramos la sección del cuadro de texto encriptado
         seccionConMensaje.style.display = "block";
         document.getElementById("btn_limpiar").disabled = false;
         document.getElementById("btn_encriptar").disabled = false;
         document.getElementById("btn_desencriptar").disabled = false;
-        //document.getElementById("btn_guardar").disabled = false;
     }
 }
-
